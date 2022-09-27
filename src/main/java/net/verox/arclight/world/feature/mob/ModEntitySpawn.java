17e4d662cd04470919f04999f834f5d860161cd4
Joel Rehra <joelrehra@gmx.de>
2022-09-27 09:44:28 +0200

    Switch to 1.19 compatible apis

--- src/main/java/net/verox/arclight/world/feature/mob/ModEntitySpawn.java
+++ src/main/java/net/verox/arclight/world/feature/mob/ModEntitySpawn.java
@@ -2,6 +2,7 @@ package net.verox.arclight.world.feature.mob;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.mob.FlyingEntity;
@@ -13,7 +14,7 @@ public class ModEntitySpawn {
         BiomeModifications.addSpawn(BiomeSelectors.foundInTheEnd(),
                 SpawnGroup.CREATURE, EntityTypes.JELLY, 2, 1, 4);
 
-        SpawnRestriction.register(EntityTypes.JELLY, SpawnRestriction.Location.NO_RESTRICTIONS,
+        SpawnRestrictionAccessor.callRegister(EntityTypes.JELLY, SpawnRestriction.Location.NO_RESTRICTIONS,
                 Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, FlyingEntity::canMobSpawn);
     }
 }
